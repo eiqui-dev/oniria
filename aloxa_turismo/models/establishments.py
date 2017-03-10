68eb13fb4929a6b7f6405582fd793fc39589bfa4
Dario Lodeiros <dario@aloxa.eu>
2017-03-10 11:46:20 +0100

    add more translates

--- aloxa_turismo/models/establishments.py
+++ aloxa_turismo/models/establishments.py
@@ -135,7 +135,7 @@ class establishment(models.Model):
     tripadvisor_url = fields.Char(string="TripAdvisor Url", size=255)
     languages = fields.Many2many('turismo.language.tag', string='Languages')
     region = fields.Many2many('turismo.region.tag', string='Region')
-    type = fields.Selection([('winecellar','Winecellar'), ('restaurant','Restaurant'),('lodging','Lodging'),('cultural','Art and Culture'),('winebar','Winebar'), ('vinhedo','Viñedo'),('other','Other')],'Establishment Type', translate=True, required=True)    
+    type = fields.Selection([('winecellar','Winecellar'), ('restaurant','Restaurant'),('lodging','Lodging'),('cultural','Art and Culture'),('winebar','Winebar'), ('vineyard','Vineyard'),('other','Other')],'Establishment Type', translate=True, required=True)    
     web = fields.Char(string="Web", size=255, default=default_web)
     website_published = fields.Boolean('WebSite Published', copy=False)
     images = fields.One2many('establishment.images', 'establishment_tmpl_id',string='Images')
